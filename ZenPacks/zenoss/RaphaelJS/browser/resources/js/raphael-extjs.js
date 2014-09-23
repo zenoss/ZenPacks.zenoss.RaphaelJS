fc8c876faa12edbb88939e427a88415500916149
Ian Wills <iwills@zenoss.com>
2014-09-23 13:12:23 -0500

    Updates for RM 424

--- ZenPacks/zenoss/RaphaelJS/browser/resources/js/raphael-extjs.js
+++ ZenPacks/zenoss/RaphaelJS/browser/resources/js/raphael-extjs.js
@@ -1,5 +1,38 @@
 // RAPHAEL RENDERING WINDOW STUFF:
+Ext.define('Ext.ux.Raphael', {
+    extend: "Ext.Component",
+    alias: ['widget.raphaelpanel'],
+    initComponent: function() {
+        var me = this;
+        this.callParent(arguments);
+    },
+    afterRender: function(ct) {
+        this.callParent(arguments);
+        var p = this.paper = Raphael(this.el.dom), v;
+        this.canvasEl = Ext.get(p.canvas);
 
+//      Export all methods from this paper object which will not override our native
+//      methods like setSize etc.
+        for (var prop in p) {
+            v = p[prop];
+            if (!this[prop] && Object.prototype.hasOwnProperty.call(p, prop) && Ext.isFunction(v)) {
+                this[prop] = Ext.bind(v, p);
+            }
+        }
+
+//      We always cache our size
+        this.cacheSizes = true;
+    },
+
+    getWidth: function() {
+        return this.canvasEl.getWidth();
+    },
+
+    getHeight: function() {
+        return this.canvasEl.getHeight();
+    }
+});
+/*
 Ext.ux.Raphael = Ext.extend(Ext.BoxComponent, {
     onRender: function(ct) {
         var p = this.paper = Raphael(ct.dom), v;
@@ -32,3 +65,4 @@ Ext.ux.Raphael = Ext.extend(Ext.BoxComponent, {
 });
 Ext.reg('raphael', Ext.ux.Raphael);
 // end of file
+*/
